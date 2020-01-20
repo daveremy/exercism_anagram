4239b7876ffcdd5f42ec2c3b2477d5c08080c635
Dave Remy <email@daveremy.com>
2020-01-20 17:37:24 +0000

    changed to use sorting approach rather than attempting to add unicode values

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,18 @@
 use std::collections::HashSet;
 
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &'a [&str]) -> HashSet<&'a str> {
-    let word_num = to_number(word);
+    let sorted_chars = sort(&mut word.to_lowercase());
     possible_anagrams
         .into_iter()
-        .filter(|w| word_num == to_number(w) && word != **w)
+        .filter(|w| {
+            word.to_lowercase() != *w.to_lowercase() && sorted_chars == sort(&mut w.to_lowercase())
+        })
         .map(|w| *w)
         .collect()
 }
 
-/// Convert the word to a unique number:
-fn to_number(word: &str) -> u64 {
-    word.chars().fold(0, |acc, c| {
-        acc + c
-            .to_string()
-            .as_bytes()
-            .iter()
-            // create a unique number for each character by multiplying each
-            // byte by an increasing magnitude in order to assure ordering of
-            // bytes matter
-            .fold((0, 1), |acc, b| (acc.1 * *b as u64, acc.1 * 10))
-            .0
-    })
+fn sort(w: &mut String) -> Vec<char> {
+    let mut v: Vec<char> = w.chars().collect();
+    v.sort();
+    v
 }
