8638e339f6590394e1571b4340d04358dcbb33d8
Dave Remy <email@daveremy.com>
2020-02-09 12:31:22 +0100

    Change to use character frequency approach

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,24 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &'a [&str]) -> HashSet<&'a str> {
-    let sorted_chars = sort(&mut word.to_lowercase());
+    let word_lower = &word.to_lowercase();
+    let word_char_freq = char_freq(word_lower);
     possible_anagrams
         .into_iter()
-        .filter(|w| {
-            word.to_lowercase() != *w.to_lowercase() && sorted_chars == sort(&mut w.to_lowercase())
+        .filter(|possible| {
+            let possible_lower = &possible.to_lowercase();
+            word.len() == possible.len()
+                && word_lower != possible_lower
+                && word_char_freq == char_freq(possible_lower)
         })
         .map(|w| *w)
         .collect()
 }
 
-fn sort(w: &mut String) -> Vec<char> {
-    let mut v: Vec<char> = w.chars().collect();
-    v.sort();
-    v
+fn char_freq(w: &str) -> HashMap<char, u64> {
+    w.chars().fold(HashMap::new(), |mut char_freq, c| {
+        let count = char_freq.entry(c).or_default();
+        *count += 1;
+        char_freq
+    })
 }
